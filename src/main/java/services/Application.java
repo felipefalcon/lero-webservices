7ab6444c6d66f609687916512daa65dd6a1a97bc
Felipe Carneiro <felipefalconwolf@hotmail.com>
2021-07-25 16:31:55 -0300

    Serviço de login com encriptação dupla concluído

--- src/main/java/services/Application.java
+++ src/main/java/services/Application.java
@@ -2,21 +2,8 @@ package services;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
-@Controller
 @SpringBootApplication
 public class Application {
-
-	@RequestMapping("/")
-	@ResponseBody
-	String home() {
-		return "LeRo webservices iniciado";
-	}
-
-	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);
-	}
+	public static void main(String[] args) { SpringApplication.run(Application.class, args); }
 }
